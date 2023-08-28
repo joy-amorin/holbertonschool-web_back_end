14e2bf1b1929d0fa21307f88d4fbafe59b2b6788
Joy-Amorin <amorinkaren2021@gmail.com>
2023-08-28 15:02:28 -0700

    fix argument add asyncio.run

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,9 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10.0) -> float:
-    delay_time = random.uniform(0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    delay_time: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay_time)
     return delay_time
+
+asyncio.run(wait_random())
