7e75c35ea5812fd8ee528dccf984c7eaab034b82
Joy-Amorin <amorinkaren2021@gmail.com>
2023-08-28 09:02:55 -0700

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """that takes a float n as argument and
+    returns the string representation of the float"""
+    return str(n)
