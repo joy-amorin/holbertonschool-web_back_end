f6aa45c111b88c49cb7dfc0e5bdf2eb432978e3a
Joy-Amorin <amorinkaren2021@gmail.com>
2023-10-06 14:24:26 -0700

    --fix

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,8 @@
 export default function getStudentIdsSum(studentsList) {
   const initilalValue = 0;
 
-  const sumValue = studentsList.reduce((accumulator, student) => accumulator + 
-  student.id, initilalValue);
+  const sumValue = studentsList.reduce((accumulator, student) => accumulator + student.id,
+  initilalValue);
 
   return sumValue;
-}  
+}
