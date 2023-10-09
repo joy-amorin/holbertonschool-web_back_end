995014b03527a26e6aa3c99dfe3bcd9967c83963
Joy-Amorin <amorinkaren2021@gmail.com>
2023-10-09 06:22:35 -0700

    8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const resultArray = [...set].map((value) => {
+    if (value.startsWith(startString)) {
+      return value.substring(startString.length);
+    }
+    return '';
+  });
+    // filterArray: remove the hypen at the end of the string
+  const filterArray = resultArray.filter((value) => value !== '');
+  return filterArray.join('-');
+}
